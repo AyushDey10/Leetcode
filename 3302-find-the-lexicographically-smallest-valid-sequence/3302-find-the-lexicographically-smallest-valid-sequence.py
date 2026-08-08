0db73455fcd9ea00d77368a94abfc8a88b0cc6fa
Ayush Dey <143836776+AyushDey10@users.noreply.github.com>
2026-08-09 02:43:14 +0530

    Time: 773 ms (71.43%), Space: 51.4 MB (57.14%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def validSequence(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: List[int]
+        """
+        n = len(word1)
+        m = len(word2)
+
+        suffix = [0] * (n + 1)
+
+        j = m - 1
+        count = 0
+
+        for i in range(n - 1, -1, -1):
+            if j >= 0 and word1[i] == word2[j]:
+                j -= 1
+                count += 1
+
+            suffix[i] = count
+
+        ans = []
+        j = 0
+        changed = False
+
+        for i in range(n):
+            if j == m:
+                break
+
+            if word1[i] == word2[j]:
+                ans.append(i)
+                j += 1
+
+            elif not changed:
+                remaining = m - j - 1
+
+                if suffix[i + 1] >= remaining:
+                    ans.append(i)
+                    j += 1
+                    changed = True
+
+        if j == m:
+            return ans
+
+        return []
+
+sol = Solution()
+
+print(sol.validSequence("vbcca", "abc"))
+print(sol.validSequence("bacdc", "abc"))
+print(sol.validSequence("aaaaaa", "aaabc"))
+print(sol.validSequence("abc", "ab"))        
